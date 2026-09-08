344a264b841a0de37ea0bc4f2e56ec223525db3d
EduardoJordao <eduardosarnejordao@gmail.com>
2026-09-08 20:24:21 -0300

    script Cinematch

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+const formulario = document.querySelector("#formulario");
+const resultado = document.querySelector("#resultado");
+
+formulario.addEventListener("submit", function (evento) {
+  evento.preventDefault();
+
+  const filmeSerie = document.querySelector("#filme-serie").value;
+  const nome = document.querySelector("#nome").value;
+  const genero = document.querySelector("#genero").value;
+  const canaisApp = document.querySelector("#canais-app").value;
+
+  resultado.textContent =
+    `Obrigado, ${nome}! Você prefere ${filmeSerie}, do gênero ${genero}, pelo ${canaisApp}.`;
+
+  formulario.reset();
+});
