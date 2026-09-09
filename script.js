3b8c0fa07a6bb95f0ead2d6e4a3345e2ddeb4fe8
EduardoJordao <eduardosarnejordao@gmail.com>
2026-09-09 12:24:43 -0300

    Fix event listener syntax in script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const resultado = document.querySelector("#resultado");
 const modal = document.querySelector("#modalResultado");
 const fecharModal = document.querySelector("#fecharModal");
 
-formulario.addEventListener("submit", function (evento)
-  evento.preventDefault();{
+formulario.addEventListener("submit", function (evento) {
+  evento.preventDefault();
 
   const nome = document.querySelector("#nome").value;
   const genero = document.querySelector("#genero").value;
@@ -15,11 +15,9 @@ formulario.addEventListener("submit", function (evento)
 
   resultado.textContent = `Obrigado, ${nome}! Você assiste sempre ${filmeserie}, você prefere filmes/séries de ${genero}. Seu canal/aplicativo favorito é ${canaisapp}, seu filme favorito é ${filme} e sua preferência de áudio é ${audio}.`;
 
-  // Abre a janela pra expor os resultados
   modal.showModal();
 });
 
-// Fecha a janela quando clicar no botão fechar
 fecharModal.addEventListener("click", function () {
   modal.close();
 });
