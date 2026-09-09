129bff290ce7afbd7d7fe2a1a76115ac3258a531
EduardoJordao <eduardosarnejordao@gmail.com>
2026-09-09 07:18:44 -0300

    Script Aplicado

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-
 const formulario = document.querySelector("#formulario");
 const resultado = document.querySelector("#resultado");
+const modal = document.querySelector("#modalResultado");
+const fecharModal = document.querySelector("#fecharModal");
 
 formulario.addEventListener("submit", function (evento) {
-  evento.preventDefault();
+evento.preventDefault();
 
-  const filmeSerie = document.querySelector("#filme-serie").value;
-  const nome = document.querySelector("#nome").value;
-  const genero = document.querySelector("#genero").value;
-  const canaisApp = document.querySelector("#canais-app").value;
+const nome = document.querySelector("#nome").value;
+const genero = document.querySelector("#genero").value;
+const audio = document.querySelector("#audio").value;
+const filme = document.querySelector("#filme").value;
+const filmeserie = document.querySelector("#filmeserie").value;
+const canaisapp = document.querySelector("#canaisapp").value;
 
-  resultado.textContent =
-    `Obrigado, ${nome}! Você prefere ${filmeSerie}, do gênero ${genero}, pelo ${canaisApp}.`;
+resultado.textContent = 'Obrigado, ${nome}! Você vê sempre ${filmeserie}, você prefere filmes/séries de ${genero}. Seu canal/aplicativo favorito é ${canaisapp}, seu filme favorito é ${filme} e prefere ${audio}!';
 
-  formulario.reset();
+// Abre a janela pra expor os resultados
+modal.showModal();
+});
+
+// Fecha a janela quando clicar no botão fechar
+fecharModal.addEventListener("click", function () {
+modal.close();
 });
