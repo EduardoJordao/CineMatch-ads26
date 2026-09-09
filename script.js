612c02ee5135fec80891a175ff5b12bbb1a1a2c3
EduardoJordao <eduardosarnejordao@gmail.com>
2026-09-09 11:51:58 -0300

    Refactor modal submission and result display logic

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ const modal = document.querySelector("#modalResultado");
 const fecharModal = document.querySelector("#fecharModal");
 
 formulario.addEventListener("submit", function (evento) {
-evento.preventDefault();
+  evento.preventDefault();
 
-const nome = document.querySelector("#nome").value;
-const genero = document.querySelector("#genero").value;
-const audio = document.querySelector("#audio").value;
-const filme = document.querySelector("#filme").value;
-const filmeserie = document.querySelector("#filmeserie").value;
-const canaisapp = document.querySelector("#canaisapp").value;
+  const nome = document.querySelector("#nome").value;
+  const genero = document.querySelector("#genero").value;
+  const audio = document.querySelector("#audio").value;
+  const filme = document.querySelector("#filme").value;
+  const filmeserie = document.querySelector("#filme-serie").value;
+  const canaisapp = document.querySelector("#canais-app").value;
 
-resultado.textContent = 'Obrigado, ${nome}! Você vê sempre ${filmeserie}, você prefere filmes/séries de ${genero}. Seu canal/aplicativo favorito é ${canaisapp}, seu filme favorito é ${filme} e prefere ${audio}!';
+  resultado.textContent = `Obrigado, ${nome}! Você assiste sempre ${filmeserie}, você prefere filmes/séries de ${genero}. Seu canal/aplicativo favorito é ${canaisapp}, seu filme favorito é ${filme} e sua preferência de áudio é ${audio}.`;
 
-// Abre a janela pra expor os resultados
-modal.showModal();
+  // Abre a janela pra expor os resultados
+  modal.showModal();
 });
 
 // Fecha a janela quando clicar no botão fechar
 fecharModal.addEventListener("click", function () {
-modal.close();
+  modal.close();
 });
